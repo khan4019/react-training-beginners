895eaf70bf425693163e347efbe4ac20c1ca9e40
Khan <khan4019@gmail.com>
2017-07-10 07:04:03 -0500

    Step 1 completed

--- steps/1/ema-john/src/components/App/App.js
+++ steps/1/ema-john/src/components/App/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import Header from './components/header/header';
+import Header from '../Header/Header';
 
 class App extends Component {
   render() {
